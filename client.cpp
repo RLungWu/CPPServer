1f271897e930f8e147442e578cad1fba9a4ea199
RLungWu <larry14789@gmail.com>
2024-06-29 23:40:02 +0800

    chore: Refactor server.cpp for improved readability and error handling

--- client.cpp
+++ client.cpp
@@ -1,5 +1,4 @@
 #include <stdio.h>
-#include <strings.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
@@ -7,33 +6,31 @@
 #include <fcntl.h>
 #include <sys/epoll.h>
 #include <errno.h>
-
 #include "util.h"
 
 #define MAX_EVENTS 1024
 #define READ_BUFFER 1024
 
 void setnonblocking(int fd){
-    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL, 0) | O_NONBLOCK);
+    fcntl(fd, F_SETFL, fcntl(fd, F_GETFL) | O_NONBLOCK);
 }
 
-int main(){
+int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    errif(sockfd == -1, "socket create error!!");
+    errif(sockfd == -1, "socket create error");
 
-    struct sockaddr_in server_addr;
-    bzero(&server_addr, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server_addr.sin_port = htons(8080);
+    struct sockaddr_in serv_addr;
+    bzero(&serv_addr, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    serv_addr.sin_port = htons(8888);
+
+    errif(bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket bind error");
+
+    errif(listen(sockfd, SOMAXCONN) == -1, "socket listen error");
     
-
-    errif(connect(sockfd, (sockaddr*)&server_addr, sizeof(server_addr)) == -1, "socket connect error!!");
-
-    errif(listen(sockfd, SOMAXCONN) == -1, "socket listen error!!");
-
-    int epfd = epoll_create(0);
-    errif(epfd == -1, "epoll create error!!");
+    int epfd = epoll_create1(0);
+    errif(epfd == -1, "epoll create error");
 
     struct epoll_event events[MAX_EVENTS], ev;
     bzero(&events, sizeof(events));
@@ -44,50 +41,49 @@ int main(){
     setnonblocking(sockfd);
     epoll_ctl(epfd, EPOLL_CTL_ADD, sockfd, &ev);
 
-
-    while (true) {
+    while(true){
         int nfds = epoll_wait(epfd, events, MAX_EVENTS, -1);
-        errif(nfds == -1, "epoll wait error!!");
+        errif(nfds == -1, "epoll wait error");
+        for(int i = 0; i < nfds; ++i){
+            if(events[i].data.fd == sockfd){        //新 client 連接
+                struct sockaddr_in clnt_addr;
+                bzero(&clnt_addr, sizeof(clnt_addr));
+                socklen_t clnt_addr_len = sizeof(clnt_addr);
 
-        for(int i = 0; i < nfds; ++i){ //New client connecting
-            if(events[i].data.fd == sockfd){
-                struct sockaddr_in client_addr;
-                bzero(&client_addr, sizeof(client_addr));
-                socklen_t client_addr_len = sizeof(client_addr);
-
-                int client_sockkfd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len);
-                errif(client_sockkfd == -1, "socket accept error!!");
-                printf("New client fd %d connected: %s:%d\n",client_sockkfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
+                int clnt_sockfd = accept(sockfd, (sockaddr*)&clnt_addr, &clnt_addr_len);
+                errif(clnt_sockfd == -1, "socket accept error");
+                printf("new client fd %d! IP: %s Port: %d\n", clnt_sockfd, inet_ntoa(clnt_addr.sin_addr), ntohs(clnt_addr.sin_port));
 
                 bzero(&ev, sizeof(ev));
-                ev.data.fd = client_sockkfd;
+                ev.data.fd = clnt_sockfd;
                 ev.events = EPOLLIN | EPOLLET;
-                setnonblocking(client_sockkfd);
-                epoll_ctl(epfd, EPOLL_CTL_ADD, client_sockkfd, &ev);
-            }else if (events[i].events & EPOLLIN){ //readable event
+                setnonblocking(clnt_sockfd);
+                epoll_ctl(epfd, EPOLL_CTL_ADD, clnt_sockfd, &ev);
+            } else if(events[i].events & EPOLLIN){      //可讀事件
                 char buf[READ_BUFFER];
-                ssize_t bytes_read = read(events[i].data.fd, buf, sizeof(buf));
-
-                if(bytes_read > 0){
-                    printf("message from client fd %d: %s\n", events[i].data.fd, buf);
-                    write(events[i].data.fd, buf, sizeof(buf));
-                }else if(bytes_read == -1 && ((errno == EAGAIN) || (errno == EWOULDBLOCK))){
-                    printf("readable event but no data to read: %d \n", errno);
-                    break;
-                }else if(bytes_read == 0){
-                    printf("EOF, client fd %d disconnected\n", events[i].data.fd);
-                    close(events[i].data.fd);                   //close the client fd
-                    break;
+                while(true){    //由於使用非阻塞IO，讀取客戶端buffer，一次讀取buf大小數據，直到全部讀取完畢
+                    bzero(&buf, sizeof(buf));
+                    ssize_t bytes_read = read(events[i].data.fd, buf, sizeof(buf));
+                    if(bytes_read > 0){
+                        printf("message from client fd %d: %s\n", events[i].data.fd, buf);
+                        write(events[i].data.fd, buf, sizeof(buf));
+                    } else if(bytes_read == -1 && errno == EINTR){  // 客戶端正常中斷，繼續讀取
+                        printf("continue reading");
+                        continue;
+                    } else if(bytes_read == -1 && ((errno == EAGAIN) || (errno == EWOULDBLOCK))){//非組塞IO，這個條件表示數據全部讀取完畢
+                        printf("finish reading once, errno: %d\n", errno);
+                        break;
+                    } else if(bytes_read == 0){  //EOF，客户端断开连接
+                        printf("EOF, client fd %d disconnected\n", events[i].data.fd);
+                        close(events[i].data.fd);    //關閉socket會自動將文件描述符從epoll樹上移除
+                        break;
+                    }
                 }
-            }else{
-                //printf("unknown event: %d\n", events[i].events);
+            } else{         //之後的版本實現
                 printf("something else happened\n");
             }
+        }
     }
     close(sockfd);
-
-
-    
-    }
     return 0;
 }
--- server.cpp
+++ server.cpp
@@ -1,60 +1,45 @@
-#include <stdio.h>
-#include <strings.h>
+#include <iostream>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <string.h>
 #include <unistd.h>
-
 #include "util.h"
 
-int main(){
+#define BUFFER_SIZE 1024 
+
+int main() {
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    errif(sockfd == -1, "socket create error!!");
+    errif(sockfd == -1, "socket create error");
 
     struct sockaddr_in serv_addr;
     bzero(&serv_addr, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    serv_addr.sin_port = htons(8080);
+    serv_addr.sin_port = htons(8888);
 
-    errif(bind(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket bind error!!");
-
-    errif(listen(sockfd, SOMAXCONN) == -1, "socket listen error!!"); //Start Listening
-
-    /* 
-     * Accept one client connection => use accept() function
-     */
-
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len = sizeof(client_addr);
-    bzero(&client_addr, sizeof(client_addr));
-
-    int client_sockfd = accept(sockfd, (sockaddr*)&client_addr, &client_addr_len);
-    errif(client_sockfd == -1, "socket accept error!!");
-
-    printf("new client fd %d! IP: %s Port: %d \n", client_sockfd, inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
-
+    errif(connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) == -1, "socket connect error");
+    
     while(true){
-        char buffer[1024];
-        bzero(&buffer, sizeof(buffer));
-        ssize_t read_bytes = read(client_sockfd, buffer, sizeof(buffer));
-
-        if(read_bytes > 0){
-            printf("Client fd %d says: %s\n",client_sockfd, buffer);
-            write(client_sockfd, buffer, sizeof(buffer));
-        }else if(read_bytes == 0){
-            printf("Client fd %d disconnected!\n", client_sockfd);
-            close(client_sockfd);
+        char buf[BUFFER_SIZE];  
+        bzero(&buf, sizeof(buf));
+        scanf("%s", buf);
+        ssize_t write_bytes = write(sockfd, buf, sizeof(buf));
+        if(write_bytes == -1){
+            printf("socket already disconnected, can't write any more!\n");
             break;
-        }else{
-            close(client_sockfd);
-            errif(true, "socket read error!!");
         }
-
+        bzero(&buf, sizeof(buf));
+        ssize_t read_bytes = read(sockfd, buf, sizeof(buf));
+        if(read_bytes > 0){
+            printf("message from server: %s\n", buf);
+        }else if(read_bytes == 0){
+            printf("server socket disconnected!\n");
+            break;
+        }else if(read_bytes == -1){
+            close(sockfd);
+            errif(true, "socket read error");
+        }
     }
     close(sockfd);
-
-
-
     return 0;
 }
